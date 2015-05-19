c5d47b915ad43ad14bfd7976b0ee89869b5cb2cb
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-19 14:37:05 -0400

    Print time on page load

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+(function() {
+  'use strict';
 
+var $display = document.querySelector('.display-time');
+
+
+function showTime(event){
+  var currentTime = new Date();
+  console.log(currentTime);
+}
+
+
+window.addEventListener('load', showTime, false)
+
+
+
+
+
+
+})();
