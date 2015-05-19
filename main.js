b62b02e52f0c427d3ef30383b61fb754b1b828ef
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-19 17:02:49 -0400

    Fix display time

--- main.js
+++ main.js
@@ -4,16 +4,25 @@
 //VARIABLES
 
 var currentTime;
+var $hours = document.querySelector('.hours');
+var $minutes = document.querySelector('.minutes');
+var $seconds = document.querySelector('.seconds');
 var $display = document.querySelector('.display-time');
 
 
 function startStopwatch(event){
   currentTime = new Date();
-  console.log(currentTime);
-  $display.textContent = currentTime;
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  $hours.textContent = hours;
+  $minutes.textContent = minutes;
+  $seconds.textContent = seconds;
 }
 
 window.setInterval(startStopwatch, 1000);
+
+
 
 
 //EVENT LISTENERS
